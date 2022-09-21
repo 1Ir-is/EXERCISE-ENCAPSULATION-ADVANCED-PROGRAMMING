ac47995026563dc7f038c787083dd659aecc0548
IriS <mhuynk1005@gmail.com>
2022-09-21 16:30:47 +0700

    Add file

---
+++ EXERCISE ENCAPSULATION/Program.cs
@@ -0,0 +1,18 @@
+﻿using EXERCISE_ENCAPSULATION;
+using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var length = double.Parse(Console.ReadLine());
+        var width = double.Parse(Console.ReadLine());
+        var height = double.Parse(Console.ReadLine());
+        Type boxType = typeof(Box);
+        FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+        Console.WriteLine(fields.Count());
+
+        var box = new Box(length, width, height);
+        Console.WriteLine(box);
+    }
+}
